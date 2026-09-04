c670e25031a90642a0c7aa535a8ef00871a995c1
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-09-04 13:33:30 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "https://fixed-content-qa-agent.netlify.app/",
     ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
