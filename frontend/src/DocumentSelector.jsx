0115fef7c8b5c0e7e2202433c5506fafa076c646
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-09-04 13:27:49 +0530

    Update DocumentSelector.jsx

--- frontend/src/DocumentSelector.jsx
+++ frontend/src/DocumentSelector.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_BASE = "https://fixed-content-qa-agent-3.onrender.com";
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 
 const DocumentSelector = ({ setUploaded }) => {
